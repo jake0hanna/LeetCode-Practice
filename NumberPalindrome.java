aaba9d92c555c91faead44c206d6ad8b55b2ecf9
jake0hanna <102441519+jake0hanna@users.noreply.github.com>
2023-04-16 13:16:52 -0400

    Daily Practice

---
+++ NumberPalindrome.java
@@ -0,0 +1,20 @@
+class NumberPalindrome
+{
+    public boolean isPalindrome(int x) 
+    {
+        
+        String s = ""+x;
+
+        for(int i = 0; i < s.length(); i++)
+        {
+
+            if(s.charAt(i) != s.charAt(s.length()-1-i))
+                    return false;
+
+
+        }
+
+        return true;
+        
+    }
+}
