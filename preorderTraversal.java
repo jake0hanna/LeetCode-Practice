22cf9d229bf3a3031866dc2ae54b72de475ed97c
jake0hanna <102441519+jake0hanna@users.noreply.github.com>
2023-04-21 18:01:03 -0400

    Daily Practice

---
+++ preorderTraversal.java
@@ -0,0 +1,44 @@
+public class preorderTraversal 
+{
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+
+    List<Integer> preorderList = new ArrayList<Integer>();
+
+    public List<Integer> preorderTraversal(TreeNode root) 
+    {
+
+        if(root != null)
+        {
+
+            preorderList.add(root.val);
+
+
+            preorderTraversal(root.left);
+            preorderTraversal(root.right);
+
+            
+
+        }
+
+
+        return preorderList;
+
+        
+    }
+}
