bc79d8533166c6bf147fcb2449a0b2ca3497dd67
jake0hanna <102441519+jake0hanna@users.noreply.github.com>
2023-04-23 13:37:50 -0400

    Daily Practice

---
+++ MaxDepthBST.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class MaxDepthBST
+{
+
+    public int maxDepth(TreeNode root) 
+    {
+
+        if(root != null)
+        {
+             
+            int left = maxDepth(root.left);
+            int right = maxDepth(root.right);
+
+            return Math.max(left, right) + 1;
+
+
+        }
+        else return 0;
+        
+    }
+}
