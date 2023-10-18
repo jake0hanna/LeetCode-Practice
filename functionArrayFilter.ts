4bc64c588befe81c7d57e53ec97d5057abdf8965
Jake Hanna <102441519+jake0hanna@users.noreply.github.com>
2023-10-18 10:31:10 -0400

    Practice

---
+++ functionArrayFilter.ts
@@ -0,0 +1,17 @@
+type Fn = (n: number, i: number) => any
+
+function filter(arr: number[], fn: Fn): number[] 
+{
+    let newArr: number[] = [];
+    
+    for(var i = 0; i < arr.length ; i++)
+    {
+        if(fn(arr[i], i))
+        {
+            newArr.push(arr[i]);
+        }
+    }
+
+    return newArr;
+	
+};
